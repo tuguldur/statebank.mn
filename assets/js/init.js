09162d7140a7f23830b1d4abc7b9f6bcd4692371
tuguldur <aa402115@gmail.com>
2019-08-11 20:31:06 +0800

    Currency Demo Home & Demo 70%

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,7 @@
 import { MDCRipple } from "@material/ripple";
 import { MDCTextField } from "@material/textfield";
 import { MDCTabBar } from "@material/tab-bar";
+import { MDCDataTable } from "@material/data-table";
 
 const iconButton = document.querySelectorAll(".mdc-icon-button");
 
@@ -10,6 +11,10 @@ const buttons = document.querySelectorAll(".mdc-button");
 buttons.forEach(button => new MDCRipple(button));
 const lists = document.querySelectorAll(".mdc-list-item");
 lists.forEach(list => new MDCRipple(list));
-new MDCTabBar(document.querySelector(".mdc-tab-bar"));
+const tabs = document.querySelectorAll(".mdc-tab-bar");
+tabs.forEach(tab => new MDCTabBar(tab));
 const inputs = document.querySelectorAll(".mdc-text-field");
 inputs.forEach(input => new MDCTextField(input));
+
+const tables = document.querySelectorAll(".mdc-data-table");
+tables.forEach(table => new MDCDataTable(table));
