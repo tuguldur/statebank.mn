3d68fd79a47b42fcec3be465976e11b8ecfc341b
tuguldur <aa402115@gmail.com>
2019-08-19 16:38:47 +0800

    Added Currency modal

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+(function($) {
+  // Header Carousel Change slide on Swipe
+  $("#headerCarousel").swipe({
+    swipe: function(
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction == "left") $(this).carousel("next");
+      if (direction == "right") $(this).carousel("prev");
+    },
+    allowPageScroll: "vertical"
+  });
+  // Drawer
+  $(".drawer-overlay").on("click", function() {
+    $("#drawer").removeClass("active");
+    $(".drawer-overlay").removeClass("active");
+  });
+  $("#drawer-btn").click(function() {
+    $("#drawer").addClass("active");
+    $(".drawer-overlay").addClass("active");
+  });
+  //   Currency Dialog
+  $(".currency-modal").click(function() {
+    $("#currency-modal").modal("show");
+  });
+})(window.jQuery);
